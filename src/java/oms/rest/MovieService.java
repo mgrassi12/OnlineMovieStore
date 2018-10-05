f88c55add1b647ba770917514b9d873eabd31806
hojo1998 <joeho2190@hotmail.com>
2018-10-05 14:20:52 +1000

    create first iteration of the REST function

---
+++ src/java/oms/rest/MovieService.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oms.rest;
+
+import java.io.IOException;
+import javax.servlet.ServletContext;
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+import javax.xml.bind.JAXBException;
+import oms.user.*;
+
+
+/**
+ *
+ * @author Joe Ho
+ */
+@Path("/movieStoreApp")
+public class MovieService {
+    @Context
+    private ServletContext application;
+
+    private MovieStoreApplication getMovieStoreApp() throws JAXBException, IOException, Exception {
+        synchronized (application) {
+            MovieStoreApplication movieStoreApp = (MovieStoreApplication) application.getAttribute("movieStoreApp");
+            if (movieStoreApp == null) {
+                movieStoreApp = new MovieStoreApplication();
+                movieStoreApp.setFilePath(application.getRealPath("WEB-INF/history.xml"));
+                application.setAttribute("movieStoreApp", movieStoreApp);
+            }
+            return movieStoreApp;
+        }
+    }
+
+// A function that displays everything without parameters
+    
+    @Path("users")
+    @GET
+    @Produces(MediaType.TEXT_XML)
+    public Users getUsers() throws JAXBException, IOException, Exception{
+       return getMovieStoreApp().getUsers();
+    }
+    
+    @Path("users/{email}")
+    @GET
+    @Produces(MediaType.TEXT_XML)
+    public User getUser(@PathParam("email")String email) throws JAXBException, IOException, Exception{
+       return getMovieStoreApp().getUsers().checkEmail(email);
+    }
+    
+  
+}
+
+// Movie Title function
+
+// Order Status function
+
+
--- src/java/oms/user/MovieStoreApplication.java
+++ src/java/oms/user/MovieStoreApplication.java
@@ -2,25 +2,39 @@ package oms.user;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.Serializable;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 
-public class MovieStoreApplication {
+public class MovieStoreApplication implements Serializable {
+
     private String filePath;
     private Users users;
+    
+public MovieStoreApplication() {
+        // TODO Auto-generated constructor stub
+    }
+
+    public MovieStoreApplication(String filePath, Users users) {
+        super();
+        this.filePath = filePath;
+        this.users = users;
+    }
 
     public String getFilePath() {
         return filePath;
     }
 
-    public void setFilePath(String filePath) throws JAXBException, FileNotFoundException, IOException {        
+    public void setFilePath(String filePath) throws JAXBException, FileNotFoundException, IOException {
         JAXBContext jc = JAXBContext.newInstance(Users.class);
         Unmarshaller u = jc.createUnmarshaller();
         this.filePath = filePath;
         FileInputStream fin = new FileInputStream(filePath);
-        users = (Users)u.unmarshal(fin);
+        users = (Users) u.unmarshal(fin);
         fin.close();
     }
 
@@ -30,5 +44,16 @@ public class MovieStoreApplication {
 
     public void setUsers(Users users) {
         this.users = users;
-    }        
+    }
+
+    public void updateXML(Users users, String filePath) throws Exception {
+        this.users = users;
+        this.filePath = filePath;
+        JAXBContext jc = JAXBContext.newInstance(Users.class);
+        Marshaller m = jc.createMarshaller();
+        m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+        FileOutputStream fout = new FileOutputStream(filePath);
+        m.marshal(users, fout);
+        fout.close();
+    }
 }
